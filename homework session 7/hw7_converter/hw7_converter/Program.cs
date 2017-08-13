a93de3edb049a75abd8392e0d3eac494928192fe
DESKTOP-CAIB5SO\Phan Thai Nam <phanthainam1092002@gmail.com>
2017-08-13 21:41:37 +0700

    hwss7

---
+++ homework session 7/hw7_converter/hw7_converter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hw7_converter
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int given_number = 298673;
+			int converted_number;
+			int devided_number;
+			while (true) 
+			{
+				converted_number = (given_number % 2);
+				devided_number = (given_number / 2);
+				Console.Write (converted_number);
+				given_number = devided_number;
+				if (devided_number == 0)
+				{
+					break;
+					
+				}
+
+			}
+
+		}
+	}
+}
---
+++ homework session 7/hw7_uocsochung/hw7_uocsochung/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace hw7_uocsochung
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			List<int> number_list = new List<int> ();
+			number_list.Add (50);
+			number_list.Add (200);
+			number_list.Add (10);
+			number_list.Add (150);
+			number_list.Sort();
+			List<int> uocso_cua_so_dau_tien = new List<int> ();
+			List<int> uocso_list = new List<int> ();
+			for (int b = 1; b <= number_list[0]; b++) 
+			{
+				if (number_list[0] % b == 0) 
+				{
+					uocso_cua_so_dau_tien.Add (b);
+				}
+					
+			}
+			for (int i = 1; i < 4; i++)
+			{
+				for (int a = 1; a < number_list [i]; a++) 
+				{
+					if (number_list[i] % a == 0)
+					{
+						for (int c = 0; c < uocso_cua_so_dau_tien.Count ; c++) 
+						{
+							if (a == uocso_cua_so_dau_tien [c]) 
+							{
+								uocso_list.Add (a);
+
+							}
+						}
+
+					}
+
+				}
+			}
+			Console.Write ("Ươc sô chung lơn nhât la : ");
+			int uocsolonnhat = 0;
+				for (int i = 0; i < uocso_list.Count; i++) 
+			{
+				if (uocso_list[i] > uocsolonnhat)
+				{
+					uocsolonnhat = uocso_list[i];
+				}
+
+			}
+			Console.WriteLine(uocsolonnhat);
+		}
+	}
+}
