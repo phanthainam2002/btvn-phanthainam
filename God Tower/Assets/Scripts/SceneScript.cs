3236b99276a91788e62b83e23cce310980f4578a
DESKTOP-CAIB5SO\Phan Thai Nam <phanthainam1092002@gmail.com>
2017-08-24 17:36:50 +0700

    god tower

---
+++ God Tower/Assets/Scripts/SceneScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneScript : MonoBehaviour {
+
+	public Text levelText;
+	public InputField inputField;
+	public Button submitButton;
+	public Text hintText;
+	public Button NextLevelButton;
+
+	public string levelContent = "LEVELS";
+	public string levelNumber;
+	public string levelAnswer;
+	string answer;
+
+	// Use this for initialization
+	void Start () {
+		NextLevelButton.gameObject.SetActive (false);
+		levelText.text = levelContent;
+		StartCoroutine (ChangeLevelTextRoutine());
+
+
+	}
+	public void nextLevelbutton(){
+		SceneManager.LoadScene("Level"+" "+ (int.Parse(levelNumber) + 1));
+		NextLevelButton.gameObject.SetActive (false);
+		
+	}
+	public void GetInput(){
+		answer = inputField.text;
+		CheckAnswer ();
+
+	}
+	public void CheckAnswer(){
+		if (answer == levelAnswer) {
+			hintText.color = Color.black;
+			hintText.text = "Congrat bro :<<, You passed level" + levelNumber ;
+			NextLevelButton.gameObject.SetActive (true);
+			//TODO : ChangeScene
+		} else {
+			hintText.text ="WRONNGGGG !!!";
+			hintText.color = Color.red;
+			inputField.text = "";
+			inputField.ActivateInputField ();
+		}
+	}
+
+	IEnumerator ChangeLevelTextRoutine(){
+		while (true) {
+			levelText.text = levelContent;
+			//waait 2 secs;
+			yield return new WaitForSeconds (2f);
+			Debug.Log ("QUSAwhauhsn");
+			levelText.text = levelNumber;
+			yield return new WaitForSeconds (2f);
+
+
+		}
+	}
+	public void startscene() {
+		SceneManager.LoadScene ("Level 1");
+	}
+	
+	// Update is called once 		per frame
+	void Update () {
+		
+	}
+}
---
+++ God Tower/Assets/Scripts/Start.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Start : MonoBehaviour {
+
+	public void beginButton(){
+		SceneManager.LoadScene ("level 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
